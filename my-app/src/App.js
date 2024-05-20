879d3462e716efe636380ff93b6209ef5950f655
root <root@TAKUYA-ISHII.localdomain>
2024-05-20 16:41:53 +0800

    Convert my-app from submodule to regular directory

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  const sum = 1 + 2 + 3;
+  const message = 'helloworld';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>1+2+3={sum}={message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
